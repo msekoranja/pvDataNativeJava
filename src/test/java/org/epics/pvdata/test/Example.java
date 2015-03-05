aba0023e8e2fb22905f394f5a1de7dc7d81ac5ab
Matej Sekoranja <matej.sekoranja@cosylab.com>
2015-03-05 15:09:57 +0100

    tests

--- src/test/java/org/epics/pvdata/test/Example.java
+++ src/test/java/org/epics/pvdata/test/Example.java
@@ -1,7 +1,6 @@
 package org.epics.pvdata.test;
 
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 import org.epics.pvdata.PVData;
 
@@ -9,30 +8,16 @@ public class Example {
 
 	public static class TimeStamp
 	{
-		long secodnds;
+		long seconds;
 		int nanos;
-		int userTag;
-
-		@Override
-		public String toString() {
-			return "TimeStamp [secodnds=" + secodnds + ", nanos=" + nanos
-					+ ", userTag=" + userTag + "]";
-		}
 	}
 
 	public static class MyData
 	{
 		double x, y;
 		String name;
-		int[] array;
+		int[] numbers;
 		TimeStamp time;
-
-		@Override
-		public String toString() {
-			return "MyData [x=" + x + ", y=" + y + ", name=" + name
-					+ ", array=" + Arrays.toString(array) + ", time=" + time
-					+ "]";
-		}
 	}
 	
 	public static void main(String[] args)
@@ -40,22 +25,19 @@ public class Example {
 		MyData data = new MyData();
 		data.x = 12.3;
 		data.y = 4.56;
-		data.name = "jure";
-		data.array = new int[] { 12, 8 };
+		data.name = "Matej";
+		data.numbers = new int[] { 1, 2, 8 };
 		data.time = new TimeStamp();
-		data.time.secodnds = 1234567890;
+		data.time.seconds = 1234567890;
 		data.time.nanos = 54321;
-		data.time.userTag = 1208;
 		
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
 		PVData.serialize(buffer, data);
 		
 		buffer.flip();
 		
+		@SuppressWarnings("unused")
 		MyData data2 = PVData.deserialize(buffer, MyData.class);
-		System.out.println(data2);
-
-		
 	}
 
 }
--- src/test/java/org/epics/pvdata/test/PVDataTest.java
+++ src/test/java/org/epics/pvdata/test/PVDataTest.java
@@ -1,6 +1,7 @@
 package org.epics.pvdata.test;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 import org.epics.pvdata.PVData;
 
@@ -80,5 +81,186 @@ public class PVDataTest extends TestCase {
 		AllPrimitives data2 = PVData.deserialize(buffer, AllPrimitives.class);
 		
 		assertEquals(data, data2);
+	}
+	
+	
+	public static class AllPrimitiveArrays {
+		boolean[] f1;
+		byte[] f2;
+		short[] f3;
+		int[] f4;
+		long[] f5;
+		float[] f6;
+		double[] f7;
+		String[] f8;
+		
+		char[] f9;
+		int check;
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			AllPrimitiveArrays other = (AllPrimitiveArrays) obj;
+			if (check != other.check)
+				return false;
+			if (!Arrays.equals(f1, other.f1))
+				return false;
+			if (!Arrays.equals(f2, other.f2))
+				return false;
+			if (!Arrays.equals(f3, other.f3))
+				return false;
+			if (!Arrays.equals(f4, other.f4))
+				return false;
+			if (!Arrays.equals(f5, other.f5))
+				return false;
+			if (!Arrays.equals(f6, other.f6))
+				return false;
+			if (!Arrays.equals(f7, other.f7))
+				return false;
+			if (!Arrays.equals(f8, other.f8))
+				return false;
+			if (!Arrays.equals(f9, other.f9))
+				return false;
+			return true;
+		}
+
+	}
+	
+	public void testPrimitiveArrays()
+	{
+		AllPrimitiveArrays data = new AllPrimitiveArrays();
+		data.f1 = new boolean[] { true, false, true };
+		data.f2 = new byte[] { 10, 0, -10 };
+		data.f3 = new short[] { 1000, 0, -1000 };
+		data.f4 = new int[] { 1000000000, 0, -1000000000 };
+		data.f5 = new long[] { 1000000000000000000L, 0, -1000000000000000000L};
+		data.f6 = new float[] { 12.34f, 0f, -12.34f };
+		data.f7 = new double[] { 567.89, 0, -567.89 };
+		data.f8 = new String[] { "hello", "world", "!" };
+		data.f9 = new char[] { 'A', 'b', 'C' };
+		data.check = 0x12345678;
+		
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		PVData.serialize(buffer, data);
+		
+		buffer.flip();
+		
+		AllPrimitiveArrays data2 = PVData.deserialize(buffer, AllPrimitiveArrays.class);
+		
+		assertEquals(data, data2);
+	}
+	
+	
+	public static class Nested3 {
+		String f1;
+		int check;
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Nested3 other = (Nested3) obj;
+			if (check != other.check)
+				return false;
+			if (f1 == null) {
+				if (other.f1 != null)
+					return false;
+			} else if (!f1.equals(other.f1))
+				return false;
+			return true;
+		}
+
+	}
+
+	public static class Nested2 {
+		double f1;
+		Nested3 f2; 
+		int check;
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Nested2 other = (Nested2) obj;
+			if (check != other.check)
+				return false;
+			if (Double.doubleToLongBits(f1) != Double
+					.doubleToLongBits(other.f1))
+				return false;
+			if (f2 == null) {
+				if (other.f2 != null)
+					return false;
+			} else if (!f2.equals(other.f2))
+				return false;
+			return true;
+		}
+		
+	}
+
+	public static class Nested1 {
+		int f1;
+		Nested2 f2; 
+		int check;
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Nested1 other = (Nested1) obj;
+			if (check != other.check)
+				return false;
+			if (f1 != other.f1)
+				return false;
+			if (f2 == null) {
+				if (other.f2 != null)
+					return false;
+			} else if (!f2.equals(other.f2))
+				return false;
+			return true;
+		}
+	}
+
+	public void testNested()
+	{
+		Nested3 n3 = new Nested3();
+		n3.f1 = "nested3";
+		n3.check = 0x12345678;
+		
+		Nested2 n2 = new Nested2();
+		n2.f1 = 12.8;
+		n2.f2 = n3;
+		n2.check = 0x87654321;
+		
+		Nested1 n1 = new Nested1();
+		n1.f1 = 0x11223344;
+		n1.f2 = n2;
+		n1.check = 0xFFFFFFFF;
+		
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		PVData.serialize(buffer, n1);
+		
+		buffer.flip();
+		
+		Nested1 data = PVData.deserialize(buffer, Nested1.class);
+		
+		assertEquals(n1, data);
+		
 	}
 }
